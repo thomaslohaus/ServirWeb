573594869da4ac2e5c7e8168c38010d67780574e
Thomas Lohaus <thomas.lohaus@gmail.com>
2017-07-10 22:17:03 -0300

    restart

--- ServirWeb/src/main/webapp/resources/js/servir.js
+++ ServirWeb/src/main/webapp/resources/js/servir.js
@@ -1,29 +1,3 @@
 /**
  * Javascript
  */
-$(document).ready(function(){
-	$('#user-menu-img').on('click', function() {
-		if ($('#user-info-menu').is(':visible'))
-			hideUserMenu();
-		else
-			showUserMenu();
-	});
-	
-	$(function () {
-	  $('[data-toggle="tooltip"]').tooltip()
-	});
-	
-	function hideUserMenu() {
-		$('#user-info-menu').hide();
-	};
-	 
-	function showUserMenu() {
-		$('#user-info-menu').show();
-	};
-	
-	$('.input-checkbox-doenca').blur(function() {
-		alert('oi');
-		alert($(this).val());
-	});
-	
-});
